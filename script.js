8d422a22921e4dce4a02363f995514717c312a5e
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-03-23 19:02:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+const circles = document.querySelectorAll('.circle');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+let currentStep = 1;
+
+nextButton.addEventListener('click', () => {
+    if (currentStep < circles.length) {
+        circles[currentStep - 1].classList.remove('active');
+        circles[currentStep].classList.add('active');
+        currentStep++;
+        prevButton.disabled = false;
+    }
+
+    if (currentStep === circles.length) {
+        nextButton.disabled = true;
+    }
+});
+
+prevButton.addEventListener('click', () => {
+    if (currentStep > 1) {
+        circles[currentStep - 1].classList.remove('active');
+        circles[currentStep - 2].classList.add('active');
+        currentStep--;
+        nextButton.disabled = false;
+    }
+
+    if (currentStep === 1) {
+        prevButton.disabled = true;
+    }
+});
